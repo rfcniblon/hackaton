ad571fe552ca1d25cffde15d371d1ddefd2167ef
rfcniblon <lambinet.nicolas@orange.fr>
2019-10-30 00:30:30 +0100

    ajout lien fontawesont

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from "react-router-dom";
+import Header from './components/Header';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Switch>
+        <Route exact path="/" component={Header} />
+       
+      </Switch>
+    
     </div>
   );
 }
 
 export default App;
+//<Route path="/ContactUs" component={ContactForm} />
+//<Route path="/erreur404" component={Erreur404} />
+   //     <Route path="/Team" component={Team} />
