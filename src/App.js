06aba90a8a235ac377b661b365fd1daefd2aebbc
Fg-35 <fabiengiboire17@gmail.com>
2019-10-30 23:34:29 +0100

    n°2 navbar fini

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
-// import { Switch, Route } from "react-router-dom";
+import './components/css/reset.css';
 import Navbar1 from './components/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import './Navbar.css';
-import {Nav,
-    Button,
-    Navbar,
-    NavDropdown,
-    Form,
-    FormControl,
-} from 'react-bootstrap';
+
 
 class Navbar1 extends React.Component {
     constructor(props) {
@@ -14,33 +8,12 @@ class Navbar1 extends React.Component {
     }
     render() {
         return (
-            <Navbar className="Navbar">
-                <Navbar.Brand>
-                    <img
-                        src="/imagesNavbar/verrecrane.jpg"
-                        className="logo"
-                        alt="logo"
-                    />
-                </Navbar.Brand>
-                <Navbar.Brand ><h1>Movie drinks</h1></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        {/* <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown> */}
-                    </Nav>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="search" />
-                        <Button variant="danger" className="buttonSearch">Search</Button>
-                    </Form>
-                </Navbar.Collapse>
-            </Navbar>
+            <div className="container">
+                <div className="navbar">
+                    <img className="logo mob-2 tab-2 dsk-2" src="/imagesNavbar/verrecrane.jpg" alt="" />
+                    <p className="title1 mob-8 tab-8 dsk-8">Movie drinks</p>
+                </div>
+            </div>
 
         );
     }
